829e425a3ff60ecb7d560ffc3fea486406f16239
glebg <gleb.grigorev.140@mail.ru>
2022-09-23 17:28:26 +0300

    second exercise

--- 3/2.py
+++ 3/2.py
@@ -4,3 +4,5 @@ second_value = float(input("Введите второе число: "))
 third_value = float(input("Введите третье число: "))
 arif_summ = (first_value+second_value+third_value)/3
 print("Арифметическая сумма равна:", round(arif_summ, 3))
+
+
