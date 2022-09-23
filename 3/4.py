da90a405c12cb0d54b4fa1b9b57a27e2c1e18e6b
glebg <gleb.grigorev.140@mail.ru>
2022-09-23 17:10:18 +0300

    third exercise

---
+++ 3/4.py
@@ -0,0 +1,8 @@
+#Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+first_value = int(input("Введите первое число: "))
+second_value = int(input("Введите второе число: "))
+third_value = int(input("Введите третье число: "))
+if first_value<0:
+    negation = first_value
+else:
+    positive = first_value
