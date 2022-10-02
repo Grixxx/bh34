1c0bab0da3df388c90ca7db7e78f191da4668487
glebg <gleb.grigorev.140@mail.ru>
2022-10-03 00:40:48 +0300

    4 exercise

--- 6/4.py
+++ 6/4.py
@@ -1,4 +1,5 @@
 #Дан список содержащий в себе различные типы данных, отфильтровать таким образом, чтобы остались только строки, использование дополнительного списка незаконно
-list_my = ['1', 'hello', '4.3', 'hfhfhf' ]
-list_my = list(filter(lambda x: x.isalpha(), list_my))
-print(list_my)
+list_my = [1, 'hello', True, 'fsfsf' ]
+list_my = map(str, list_my)
+result = list(filter(lambda x: x.isalpha(), list_my))
+print(result)
